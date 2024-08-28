cb8442e972a64d1662f4153d3909b946e5dc3502
Avish <avitmr2345@gmail.com>
2024-08-28 08:38:35 +0530

    handleGetAnalytics route added

--- controllers/url.js
+++ controllers/url.js
@@ -15,4 +15,13 @@ async function handleGenerateShortURL(req, res) {
   return res.json({ id: shortID });
 }
 
-export { handleGenerateShortURL };
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+
+export { handleGenerateShortURL, handleGetAnalytics };
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { handleGenerateShortURL } from "../controllers/url.js";
+import {
+  handleGenerateShortURL,
+  handleGetAnalytics,
+} from "../controllers/url.js";
 const router = express.Router();
 
 router.post("/", handleGenerateShortURL);
+router.get("/analytics/:shortId", handleGetAnalytics);
 
 export default router;
